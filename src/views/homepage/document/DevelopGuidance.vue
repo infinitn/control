57e8bca5952a946baf16f8f51325b969ac602801
侯立源 <hou_liy@163.com>
2021-01-28 16:48:41 +0800

    提交

--- src/views/homepage/document/DevelopGuidance.vue
+++ src/views/homepage/document/DevelopGuidance.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
     开发指引
+    <v-chart :options="options"/>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      options: {
+        xAxis: {
+          type: 'category',
+          data: [1, '2', '3', '4', '5', '6']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: [100, 500, 300, 400, 800, 10000],
+          type: 'line'
+        }]
+      }
+    }
+  }
+}
+</script>
